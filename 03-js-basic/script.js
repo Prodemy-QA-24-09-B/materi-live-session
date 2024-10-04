718cd478461d5200a995fe5db79200926d848d7f
Hairunisa <143789547+hairunisa0929@users.noreply.github.com>
2024-10-04 11:31:27 +0700

    day 3 live session

---
+++ 03-js-basic/script.js
@@ -0,0 +1,41 @@
+// console.log("Hello world")
+// alert("Good bye world");
+
+let animal = "Cat";
+// let animal = "Chicken";
+// animal = "Rabbit";
+const fruit = "Apple";
+// const fruit = "Mango";
+// console.log(fruit);
+
+let a = 5;
+let b = 7.7;
+// console.log(a * b);
+
+let kosong = null;
+// console.log(kosong);
+
+let x;
+// console.log(x)
+
+function greeting() {
+    console.log("Halo")
+}
+
+// greeting();
+
+// with parameter
+function sayTheName(firstName, lastName) {
+    console.log(firstName, lastName);
+};
+
+// sayTheName("Kucing", "Oren");
+
+// with return
+function perkalian(a, b) {
+    return a*b
+}
+
+const result = perkalian(5, 7);
+console.log(result);
+
